84cb88b7ea64386e1d75a19d2d723893edce5b51
JHKim <zhwan85@gmail.com>
2017-10-15 00:13:19 +0900

    .
    .

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname+"/public"));
 app.get('/', function (req, res) {
     console.log('Some user connected.');
     res.sendFile(__dirname + '/app/index.html');
 });
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
     console.log('Executed Server!!');
 });
